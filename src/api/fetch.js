9cef43ac7bd0bdd4361cc99cd262a4879e1084da
K. Rafalski <kr2174@caa.columbia.edu>
2023-01-10 09:21:16 -0500

    Initial commit

---
+++ src/api/fetch.js
@@ -0,0 +1,32 @@
+// Shows
+
+// Create
+export function createShow(show) {
+  return;
+}
+
+// Delete
+export function destroyShow(id) {
+  return;
+}
+
+// Index/Get all
+export function getAllShows() {
+  return;
+}
+
+// Show/Get one
+export function getOneShow(id) {
+  return;
+}
+
+// Update
+export function updateShow(id, show) {
+  return;
+}
+
+// Movies
+
+export function getAllMovies() {
+  return;
+}
---
+++ src/components/common/Footer.js
@@ -0,0 +1,9 @@
+import "./Footer.css";
+
+export default function Footer() {
+  return (
+    <footer>
+      <p>ScreenViews. An application created by YOUR_NAME.</p>
+    </footer>
+  );
+}
