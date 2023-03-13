c87ce7deaccae5829a3e8512fdb4b721b805ff4a
shaquala <96317236+shaqfred@users.noreply.github.com>
2023-03-12 23:42:30 -0400

    modified fetch.js

--- src/api/fetch.js
+++ src/api/fetch.js
@@ -2,32 +2,66 @@
 const URL = process.env.REACT_APP_BASE_URL;
 // Create
 export function createShow(show) {
-  return;
+  const method = {
+    method: "POST",
+    body: JSON.stringify(show),
+    headers: { "Content-Type": "application/json" },
+  };
+  return fetch(`${URL}/shows,method`).then((response) => response.json());
 }
 
 // Delete
 export function destroyShow(id) {
-  return;
+  return fetch(`${URL}/shows`, method).then((response) => response.json());
 }
 
 // Index/Get all
 export function getAllShows() {
-  fetch("http://localhost5001/api/shows");
+  fetch("http://localhost5001/api/shows").then((response) => response.json());
   return;
 }
 
 // Show/Get one
 export function getOneShow(id) {
-  fetch();
+  return fetch(`${URL}/shows/${id}`).then((response) => response.json());
 }
 
 // Update
 export function updateShow(id, show) {
-  return;
+  const method = {
+    method: "PUT",
+    body: JSON.stringify(show),
+    headers: { "Content-Type": "application/json" },
+  };
+  return fetch(`${URL}/shows/${id},method`).then((response) => response.json());
 }
 
 // Movies
 
-export function getAllMovies() {
-  return;
+export default function getAllMovies() {
+  return fetch(`${URL}/movies`).then((response) => response.json());
+}
+export function getOneMovie(id) {
+  return fetch(`${URL}/movies/${id}`).then((response) => response.json());
+}
+export function destroyMovie(id) {
+  return fetch(`${URL}/movies/${id}`, { method: "DELETE" });
+}
+export function createMovie(movie) {
+  const method = {
+    method: "POST",
+    body: JSON.stringify(movie),
+    headers: { "Content-Type": "application/json" },
+  };
+  return fetch(`${URL}/movies`, method).then((response) => response.json());
+}
+export function updateMovie(id, movie) {
+  const method = {
+    method: "PUT",
+    body: JSON.stringify(movie), // will create a string object of our values in JSON
+    headers: { "Content-Type": "application/json" },
+  };
+  return fetch(`${URL}/movies/${id}`, method).then((response) =>
+    response.json()
+  );
 }
